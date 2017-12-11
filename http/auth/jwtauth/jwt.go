a1facd3fdf71718c1d7a9e3666d694d4026f9592
Evan Villemez <evillemez@gmail.com>
2017-12-11 13:03:17 -0500

    wip jwt from other projects

--- http/auth/jwtauth/jwt.go
+++ http/auth/jwtauth/jwt.go
@@ -1,13 +1,48 @@
 package jwtauth
 
-/*
-type JwtAuthenticator func(token *jwt.Token) (ApiClient, error)
+import (
+	"net/http"
 
-// Look at https://github.com/auth0/go-jwt-middleware/blob/master/jwtmiddleware.go#L163
-// for inspiration
-func CreateJwtAuthenticator(keyname string, authFn JwtAuthenticator) http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
+	jwt "github.com/dgrijalva/jwt-go"
 
+	"github.com/globalprofessionalsearch/go-tools/auth"
+)
+
+// JWTAuthenticator receives a JWT token, and is expected to return an object
+// that will be stored in the request context.  If an error is returned, it's
+// encouraged to return one of the errors defined in the auth package.
+type JWTAuthenticator func(token *jwt.Token) (interface{}, error)
+
+// NewJWTAuthenticator creates a middleware that will detect an incoming
+// JWT Token in the specified location, call a user-define function for validating
+// the token, and store a returned object in the request context.
+func NewJWTAuthenticator(keyname, contextKey string, failFn auth.ErrorHandler, authFn JWTAuthenticator) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+			req, err := checkJWT(keyname, contextKey, r, authFn)
+			if err != nil {
+				failFn(rw, req, err)
+				return
+			}
+			next.ServeHTTP(rw, req)
+		})
 	}
 }
-*/
+
+// NewJWTAuthenticatorMiddleware creates a negroni-style middleware that will detect an incoming
+// JWT token in the specified location, call a user-define function for validating
+// the token, and store a returned object in the request context.
+func NewJWTAuthenticatorMiddleware(keyname, contextKey string, failFn auth.ErrorHandler, authFn JWTAuthenticator) func(http.ResponseWriter, *http.Request, http.HandlerFunc) {
+	return func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		req, err := checkJWT(keyname, contextKey, r, authFn)
+		if err != nil {
+			failFn(rw, req, err)
+			return
+		}
+		next(rw, req)
+	}
+}
+
+func checkJWT(keyname, contextKey string, r *http.Request, authFn JWTAuthenticator) (*http.Request, error) {
+
+}
